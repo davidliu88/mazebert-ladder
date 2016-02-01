2d07c73ff2182f9ca30ac1cbb6be1056120c52f8
casid <andy@mazebert.com>
2016-02-01 22:41:08 +0100

    Added run script to invoke from intellij run config.

---
+++ technical-details/docker/dev/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker-compose up -d
+
+cd ../../..
+mvn clean package -DskipTests=true
+
+cd target
+
+echo "Deploying war file."
+
+docker cp mazebert-ladder-0.1.0.war mazebert-ladder:/usr/local/tomcat/webapps/ROOT.war
