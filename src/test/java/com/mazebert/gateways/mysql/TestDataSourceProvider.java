1f90b45dfd9901adbcb1b3363d17f032596b1893
casid <andy@mazebert.com>
2016-02-06 11:53:45 +0100

    Added support to change database host for local builds.

--- src/test/java/com/mazebert/gateways/mysql/TestDataSourceProvider.java
+++ src/test/java/com/mazebert/gateways/mysql/TestDataSourceProvider.java
@@ -1,15 +1,32 @@
 package com.mazebert.gateways.mysql;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.jusecase.builders.BuilderFactory;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import static org.jusecase.builders.BuilderFactory.a;
+import static org.jusecase.builders.BuilderFactory.inputStream;
 
 public class TestDataSourceProvider extends C3p0DataSourceProvider {
     public static final TestDataSourceProvider instance = new TestDataSourceProvider();
 
     private TestDataSourceProvider() {
-        super("root", "integrationtest", "192.168.99.100/ladder_mazebert");
+        super("root", "integrationtest", resolveHost() + "/ladder_mazebert");
+    }
+
+    private static String resolveHost() {
+        String defaultHost = "localhost";
+        Properties properties = new Properties();
+        try {
+            properties.load(a(inputStream().withTestResource("integrationtest-database.properties")));
+            return properties.getProperty("host", defaultHost);
+        } catch (Throwable e) {
+            return defaultHost;
+        }
     }
 
     @Override
