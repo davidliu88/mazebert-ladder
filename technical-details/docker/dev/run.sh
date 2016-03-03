5fe243b838be55689c1c31243285007fad54ca8b
casid <andy@mazebert.com>
2016-03-03 09:04:12 +0100

    Settings file is deployed to container.

--- technical-details/docker/dev/run.sh
+++ technical-details/docker/dev/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+echo "Starting system via docker compose."
 docker-compose up -d
 
+echo "Deploying settings file."
+docker exec mazebert-ladder-dev mkdir -p /opt/mazebert
+docker cp settings.json mazebert-ladder-dev:/opt/mazebert/settings.json
+
+echo "Packaging maven project."
 cd ../../..
 mvn clean package -DskipTests=true
 
-cd target
-
 echo "Deploying war file."
-
+cd target
 docker cp mazebert-ladder-0.1.0.war mazebert-ladder-dev:/usr/local/tomcat/webapps/ROOT.war
