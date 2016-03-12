72f53ffe316390249af42bf6f0df030d0b345e24
casid <andy@mazebert.com>
2016-03-12 22:30:47 +0100

    Shorten docker container names.

--- technical-details/docker/debug/run.sh
+++ technical-details/docker/debug/run.sh
@@ -3,8 +3,8 @@ echo "Starting system via docker compose."
 docker-compose up -d
 
 echo "Deploying settings file."
-docker exec mazebert-ladder-dev mkdir -p /opt/mazebert
-docker cp settings.json mazebert-ladder-dev:/opt/mazebert/settings.json
+docker exec mbl-debug mkdir -p /opt/mazebert
+docker cp settings.json mbl-debug:/opt/mazebert/settings.json
 
 echo "Packaging maven project."
 cd ../../..
@@ -12,4 +12,4 @@ mvn clean package -DskipTests=true
 
 echo "Deploying war file."
 cd target
-docker cp mazebert-ladder-0.1.0.war mazebert-ladder-dev:/usr/local/tomcat/webapps/ROOT.war
+docker cp mbl.war mbl-debug:/usr/local/tomcat/webapps/ROOT.war
