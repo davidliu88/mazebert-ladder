4d856cf17accf6ed66501a078139c04264b3aafb
casid <andy@mazebert.com>
2016-03-13 03:18:22 +0100

    Add status to get player response.

--- src/main/java/com/mazebert/usecases/player/GetPlayers.java
+++ src/main/java/com/mazebert/usecases/player/GetPlayers.java
@@ -3,6 +3,7 @@ package com.mazebert.usecases.player;
 import com.mazebert.entities.PlayerRow;
 import com.mazebert.error.BadRequest;
 import com.mazebert.gateways.PlayerRowGateway;
+import com.mazebert.presenters.jaxrs.response.StatusResponse;
 import org.jusecase.Usecase;
 
 import javax.inject.Inject;
@@ -12,11 +13,6 @@ import java.util.List;
 @Singleton
 public class GetPlayers implements Usecase<GetPlayers.Request, List<PlayerRow>> {
     private final PlayerRowGateway playerRowGateway;
-
-    public static class Request {
-        public int start;
-        public int limit;
-    }
 
     @Inject
     public GetPlayers(PlayerRowGateway playerRowGateway) {
@@ -33,5 +29,11 @@ public class GetPlayers implements Usecase<GetPlayers.Request, List<PlayerRow>> 
         }
 
         return playerRowGateway.findPlayers(request.start, request.limit);
+    }
+
+    @StatusResponse(field = "players")
+    public static class Request {
+        public int start;
+        public int limit;
     }
 }
