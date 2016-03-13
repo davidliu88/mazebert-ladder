3d3cb4977f04f1dc9130b28e2253f6bc4f5a87fd
casid <andy@mazebert.com>
2016-03-13 03:18:37 +0100

    Allow cross origin requests from mazebert.com

---
+++ src/main/java/com/mazebert/presenters/jaxrs/filter/CrossOriginFilter.java
@@ -0,0 +1,18 @@
+package com.mazebert.presenters.jaxrs.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CrossOriginFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "https://mazebert.com");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    }
+}
