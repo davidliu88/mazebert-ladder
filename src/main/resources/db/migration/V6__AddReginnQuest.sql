49cc4de49ca228d7309fa760f01699260c485e6a
casid <andy@mazebert.com>
2016-04-23 18:58:14 +0200

    Add quest to unlock reginn

---
+++ src/main/resources/db/migration/V6__AddReginnQuest.sql
@@ -0,0 +1,2 @@
+INSERT INTO Quest (id, title, description, requiredAmount, reward, isHidden, sinceVersion) VALUES
+(28, 'Reginn the Dvergr', 'You can now forge golden cards directly!', 1, 250, 1, '1.4.0');
